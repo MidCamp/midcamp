ce8b123858040849f236ae0a3d74ab5c91e48556
Andrea Soper <soper@palantir.net>
2017-09-26 15:01:10 -0500

    Put the drush files back.

--- drushrc.php
+++
@@ -1,7 +0,0 @@
-<?php
-if (getenv('AMAZEEIO_BASE_URL')) {
-  $options['uri'] = getenv('AMAZEEIO_BASE_URL');
-}
-if (getenv('AMAZEEIO_WEBROOT')) {
-  $options['root'] = getenv('AMAZEEIO_WEBROOT');
-}
