fcd049e2e02b17e72e77075674ce0024918e2086
Andrea Soper <soper@palantir.net>
2017-12-03 21:21:49 -0600

    Make the styleguide work w/ the composer installed version of spress in the main project.

--- styleguide/conf/butler.defaults.js
+++ styleguide/conf/butler.defaults.js
@@ -1,7 +1,7 @@
 // Project-specific Butler configuration.
 var overrides = {};
 
-overrides.repo = "git@github.com:MidCamp/Hatter.git";
+overrides.repo = "https://github.com/MidCamp/midcamp.git";
 overrides.develop_tasks = ['sass', 'spress-serve', 'spress-watch', 'watch'];
 overrides.scss = ['../../src/sass/*.scss', '../../src/sass/**/*.scss'];
 overrides.css = '../../src/content/assets/css/';
@@ -9,5 +9,9 @@ overrides.template_files = ['../../src/*.html', '../../src/**/*.html'];
 overrides.output_dev = '../../build';
 overrides.html_files = ['../../build/*.html', '../../build/**/*.html'];
 overrides.output_prod = '../../build/**/*';
+
+// Where to find spress.
+overrides.spress_home = '../../';
+overrides.spress_bin = '../../../vendor/bin/spress';
 
 module.exports = overrides;
