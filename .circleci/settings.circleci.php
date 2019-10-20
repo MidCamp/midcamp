fa1fd8a96919ce0ee8afb717a767c524ff1507ad
Brian Clement <brian@bclement.com>
2019-10-20 09:37:03 -0500

    Add some db config.

---
+++ .circleci/settings.circleci.php
@@ -0,0 +1,12 @@
+<?php
+
+$databases['default']['default'] = array(
+  'database' => 'drupal',
+  'username' => 'drupal',
+  'password' => 'drupal',
+  'prefix' => '',
+  'host' => '127.0.0.1',
+  'port' => '',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+  'driver' => 'mysql',
+);
